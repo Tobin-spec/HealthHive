73e50ff975b44aff059817c865b9ff584d59beb4
Tobin Tom <tt222nb@student.lnu.se>
2023-05-19 10:52:36 +0200

    Progress

--- app/src/main/java/Client/LoginWindow.java
+++ app/src/main/java/Client/LoginWindow.java
@@ -1,23 +1,54 @@
 package Client;
 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class LoginWindow {
-    JFrame f;
+    JFrame frame;
 
     public void loginWindow(){
-        f = new JFrame();
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel userLabel = new JLabel("Username");
+        userLabel.setBounds(10,20,80,25);
+        frame.add(userLabel);
 
         JTextField userName = new JTextField();
         userName.setText("Enter your Username");
-        userName.setBounds(50,50, 150,20);
+        userName.setBounds(100,20, 165,25);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        frame.add(passwordLabel);
 
         JTextField password = new JTextField();
         password.setText("Enter your password");
-        password.setBounds(50,50, 150,20);
+        password.setBounds(100,50, 165,25);
 
         JButton login = new JButton("Login");
-        login.setBounds(25, 100, 95, 30);
+        login.setBounds(10, 80, 80, 25);
+
+        login.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mainMenu();
+            }
+        });
+
+        frame.add(userName);
+        frame.add(password);
+        frame.add(login);
+
+        frame.setVisible(true);
+
+    }
+
+    public void mainMenu() {
 
     }
 }
