e0ef3bd45cd147fdf09b0a5a6fa07c2306b14f8a
Tobin Tom <tt222nb@student.lnu.se>
2023-05-19 11:28:23 +0200

    More progress

--- app/src/main/java/Client/LoginWindow.java
+++ app/src/main/java/Client/LoginWindow.java
@@ -1,11 +1,13 @@
 package Client;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class LoginWindow {
@@ -49,6 +51,64 @@ public class LoginWindow {
     }
 
     public void mainMenu() {
+        frame = new JFrame("Main Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 1));
 
+        JButton patients = new JButton("Patient Management");
+        JButton appointments = new JButton("Appointment Management");
+        JButton inventory = new JButton("Inventory Management");
+        JButton tests = new JButton("Tests Management");
+        JButton staffs = new JButton("Staff Management");
+
+        patients.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                patientMenu();
+            }
+        });
+
+        appointments.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appointmentMenu();
+            }
+        });
+
+        inventory.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                inventoryMenu();
+            }
+        });
+
+        tests.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                testMenu();
+            }
+        });
+
+        staffs.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                staffMenu();
+            }
+        });
     }
+
+    protected void staffMenu() {
+    }
+
+    protected void testMenu() {
+    }
+
+    protected void inventoryMenu() {
+    }
+
+    protected void appointmentMenu() {
+    }
+
+    public void patientMenu() {
+    }
+
+
 }
