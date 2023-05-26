cbbe3036a42c363b2da2d80a12189935bffb2a98
Tobin Tom <tt222nb@student.lnu.se>
2023-05-26 10:35:08 +0200

    Runnable .bat file

--- app/src/main/java/Server/DataManagement.java
+++ app/src/main/java/Server/DataManagement.java
@@ -9,52 +9,62 @@ import java.util.Scanner;
 public class DataManagement {
     private HealthHive healthHive;
 
-
-    public DataManagement(HealthHive healthHive){
+    public DataManagement(HealthHive healthHive) {
         this.healthHive = healthHive;
     }
 
     public void getData() throws IOException {
+        String filepath = "data/HealthHive.data";
 
-      File file = new File("data/HealthHive.data");
-      Scanner scan = new Scanner(file, "UTF-8");
-  
-        // Skip invalid entries
-        while (scan.hasNextLine()) {
-          String line = scan.nextLine();
-          String[] params = line.split(":");
-          int dashCounter = 0;
-          for (int i = 0; i < params[0].length(); i++) {
-            if (params[0].charAt(i) == '-') {
-              dashCounter++;
+        File file = new File(filepath);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created successfully.");
+            } else {
+                System.out.println("File already exists.");
+                Scanner scan = new Scanner(file, "UTF-8");
+
+                // Skip invalid entries
+                while (scan.hasNextLine()) {
+                    String line = scan.nextLine();
+                    String[] params = line.split(":");
+                    int dashCounter = 0;
+                    for (int i = 0; i < params[0].length(); i++) {
+                        if (params[0].charAt(i) == '-') {
+                            dashCounter++;
+                        }
+                    }
+
+                    switch (dashCounter) {
+                        case 0:
+                            healthHive.addPatient(params[0], params[1], Integer.parseInt(params[2].strip()), params[3]);
+                            break;
+                        case 1:
+                            healthHive.addItem(params[0].replace("-", ""), Integer.parseInt(params[1].strip()));
+                            break;
+                        case 2:
+                            healthHive.addDoctor(params[0].replace("--", ""), params[1]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                scan.close();
             }
-          }
-  
-          switch (dashCounter) {
-            case 0:
-              healthHive.addPatient(params[0], params[1], Integer.parseInt(params[2].strip()), params[3]);
-              break;
-            case 1:
-              healthHive.addItem(params[0].replace("-", ""), Integer.parseInt(params[1].strip()));
-              break;
-            case 2:
-              healthHive.addDoctor(params[0].replace("--", ""), params[1]);
-              break;
-            default:
-              break;
-          }
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
         }
-        scan.close();
+
     }
 
     public void saveData(String content) {
-      String filePath = "data/HealthHive.data";
-  
-      try (FileWriter writer = new FileWriter(filePath, StandardCharsets.UTF_8)) {
-        writer.write(content);
-        writer.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+        String filePath = "data/HealthHive.data";
+
+        try (FileWriter writer = new FileWriter(filePath, StandardCharsets.UTF_8)) {
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
